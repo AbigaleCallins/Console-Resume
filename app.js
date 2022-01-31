7050cfb59f8f7e845d478a8246ca957b0abe3cb7
AbigaleCallins <alcallins@gmail.com>
2022-01-31 13:25:28 -0600

    adding Console Resume

---
+++ app.js
@@ -0,0 +1,43 @@
+let aName = "Abigale Callins"
+let aCareer = "Full-Stack Web Developer"
+let aObjective = "Highly motivated and detail-oriented Web Developer Candidate."
+//display Name, Career, and objective
+console.log("Name:" +" " + aName);
+console.log("Career:" + " " + aCareer);
+console.log(aObjective);
+
+//display of interests heading and list
+console.log("My Interests:")
+//list
+let aInterests = ["Hiking", "DIY", "Sewing", "Swimming", "Learning"]
+
+//list form
+for (let index = 0; index < aInterests.length; index++) {
+        console.log(aInterests[index])
+       }
+
+//experience list
+let aExperience= [
+    {coName: "Birmingham Park & Rec", coPlace: "Birmingham, AL", coTitle: "Managing Lifeguard", 
+    Duties:"Certified and trained as a professional rescuer, to ensure patrons’ safety in a family-friendly environment"},
+    {coName: "The University of Alabama at Birmingham", coPlace:"Birmingham, AL", coTitle: "Patient Transporter", 
+    Duties: "Properly and safely transported patients, secured IVs, drainage tubes, and other hospital equipment, while securing patient"},
+    {coName:"Lowes", coPlace: "Birmingham, AL", coTitle: "Retail Sales Specialist",
+    Duties: "Delivered customer-focused service that insured continued customer return"}
+];
+
+for (let j = 0; j < aExperience.length; j++) {
+    console.log = aExperience[j];    
+}
+
+
+
+let aSkills = ["Leadership experience", "Collaboration talent", "problem- soving abilities", "Adaptable", "Creative"]
+
+
+// Now the Experience div.
+
+
+
+  
+  
